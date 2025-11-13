82ec4e1a59d7e5d3d44c7e843a822e5fdf4aa1c9
Khanh Pham Tan (Sun-Asterisk) <95212299+khanhpt-2853@users.noreply.github.com>
2025-11-13 13:11:51 +0700

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
-import "@/app/ui/global.css";
-import { inter } from "@/app/ui/fonts";
-import { clsx } from "clsx";
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { clsx } from 'clsx';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
@@ -9,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={clsx(inter.className, "antialiased")}>{children}</body>
+      <body className={clsx(inter.className, 'antialiased')}>{children}</body>
     </html>
   );
 }
